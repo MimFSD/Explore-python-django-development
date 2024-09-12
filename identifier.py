6210add38dce19c81acc3487d5beb55ca3200dfa
MimFSD <mimislam878@gmail.com>
2024-09-12 09:29:39 +0600

    identifier

---
+++ identifier.py
@@ -0,0 +1,11 @@
+# letter , digit, _ , $
+
+# Valid Identifiers
+username = "Alice"          # A variable to store a string
+user_age = 30               # A variable to store an integer
+total_amount = 500.75       # A variable to store a float
+is_member = True            # A boolean variable
+
+
+
+print(username , user_age ,total_amount, is_member)
