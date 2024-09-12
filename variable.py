3e745c5086f577c8f0bb2ea7107b17bfcd2a9104
MimFSD <mimislam878@gmail.com>
2024-09-12 09:05:38 +0600

    variable

---
+++ variable.py
@@ -0,0 +1,13 @@
+# A variable is name given to a memory location in a program
+
+Name = "Arafat Mim"
+Age =73
+Marks= 87.52
+Roll = 4658 
+
+change_roll= Roll
+
+print(change_roll)
+
+print("My name is : ",Name)
+print("My age is : ", Age)
