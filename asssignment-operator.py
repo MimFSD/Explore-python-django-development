d5cb2de348edad66fee3468b18e2dbe4d2b9866c
MimFSD <mimislam878@gmail.com>
2024-09-12 10:24:01 +0600

    logical operator

--- asssignment-operator.py
+++ asssignment-operator.py
@@ -10,7 +10,14 @@ a %= 3  # a is now 1.0
 a **= 2  # a is now 1.0  Power
 a //= 1  # a is still 1.0  Integer part of division
 
-x = 10
-x //= 3  # Equivalent to x = x // 3, so x is now 3
 
 print(a)  # Output: 1.0
+
+
+x = 20
+x //= 3  # Equivalent to x = x // 3, so x is now 6     20/3= 6  Modulus =2
+print(x)
+
+w = 2
+w **= 5  # Equivalent to x = x ** 5, so x is now 64    2^5=32     
+print(w)
