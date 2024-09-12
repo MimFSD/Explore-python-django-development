89372232f4c1eda454718bb192603591af14ac88
MimFSD <mimislam878@gmail.com>
2024-09-12 10:35:59 +0600

    Type casting

---
+++ Type-Conversion.py
@@ -0,0 +1,17 @@
+a=3
+b=5.346
+print(a+b) # 3.00 +5.346 =8.346
+
+a, b = 1, 2.6547
+sum = a + b
+print(sum)
+
+
+# a, b = 1, "2"  #not allow
+# sum2 = a + b  #error
+# print(sum2)
+
+
+x,y=4,"7"
+z=int(y)   # Type casting
+print(x+z) # 4+7 =11
