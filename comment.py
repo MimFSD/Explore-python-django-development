f058af4662f874f722c0cba6aeaacfe2d5dfdc97
MimFSD <mimislam878@gmail.com>
2024-09-12 09:52:39 +0600

    comment added

---
+++ comment.py
@@ -0,0 +1,10 @@
+# Single Line comment
+
+
+
+
+
+"""  Multiple line comment
+To insert characters that are illegal in a string, use an escape character.
+
+An escape character is a backslash \ followed by the character you want to insert."""
---
+++ sum-ins.py
@@ -0,0 +1,6 @@
+a= 1300
+b=700
+add=a+b
+ins=a-b
+
+print(add,ins)
