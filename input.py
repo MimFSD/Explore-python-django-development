ddf1146df341efeca71f4a7a3873c308bedb937e
MimFSD <mimislam878@gmail.com>
2024-09-12 10:57:20 +0600

    user input

--- input.py
+++ input.py
@@ -1,2 +1,11 @@
-Name = input("Enter your name : ")
-print("welcome",Name)
+# Name = input("Enter your name : ")
+# print("welcome",Name)
+
+
+# print(input("Enter Roll : "))
+
+id =input("enter id :")
+print("your id ; " ,id , type(id))
+
+Age = int(input("Enter your age : "))
+print("your age is : ",Age  ,type(Age))
