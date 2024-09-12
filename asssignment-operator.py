745d986ec24bac47907d0ac3029137da0fb04db4
MimFSD <mimislam878@gmail.com>
2024-09-12 10:14:30 +0600

    assignment operator

---
+++ asssignment-operator.py
@@ -0,0 +1,16 @@
+# Basic assignment
+a = 5
+
+# Compound assignments
+a += 3  # a is now 8  a=a+3
+a -= 2  # a is now 6   a=a-2
+a *= 4  # a is now 24  a=a*4
+a /= 6  # a is now 4.0 (note that division always returns a float)
+a %= 3  # a is now 1.0
+a **= 2  # a is now 1.0  Power
+a //= 1  # a is still 1.0  Integer part of division
+
+x = 10
+x //= 3  # Equivalent to x = x // 3, so x is now 3
+
+print(a)  # Output: 1.0
