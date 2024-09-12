6d36aa8a9b3c88424bb64b7f3df6fee553c7c720
MimFSD <mimislam878@gmail.com>
2024-09-12 10:04:20 +0600

    relational operator

---
+++ relational-operator.py
@@ -0,0 +1,26 @@
+a = 10
+b = 20
+
+# Equal to
+print(a == b)  # Output: False
+
+# Not equal to
+print(a != b)  # Output: True
+
+# Greater than
+print(a > b)   # Output: False
+
+# Less than
+print(a < b)   # Output: True
+
+# Greater than or equal to
+print(a >= b)  # Output: False
+
+# Less than or equal to
+print(a <= b)  # Output: True
+
+# Using relational operators in an if statement
+if a < b:
+    print(f"{a} is less than {b}")
+else:
+    print(f"{a} is not less than {b}")
