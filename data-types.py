ff3bf6f9942a396dda112f8b0307043f0c414f9f
MimFSD <mimislam878@gmail.com>
2024-09-12 09:43:36 +0600

    identifier

--- data-types.py
+++ data-types.py
@@ -1,14 +1,11 @@
-# letter , digit, _ , $
-
-# Valid Identifiers
 username = "Mim RHM"          # A variable to store a string
 user_age = 30               # A variable to store an integer
 total_amount = 500.75       # A variable to store a float
 is_member = True            # A boolean variable
-
-print(username , user_age ,total_amount, is_member)
+k=None
 
 print(type(username))
 print(type(user_age))
 print(type(total_amount))
 print(type(is_member))
+print(type(k))
