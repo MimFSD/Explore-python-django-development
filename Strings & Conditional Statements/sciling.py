050ab5b9086328bb3f03396e7ae2ff8b5565dab2
MimFSD <mimislam878@gmail.com>
2024-09-13 21:26:08 +0600

    Slicing

---
+++ Strings & Conditional Statements/sciling.py
@@ -0,0 +1,17 @@
+# Negative Index
+
+text = "Python Programming"
+
+# Extracting a substring
+slice1 = text[0:6]  # From index 0 to 5
+print(slice1)  # Output: 'Python'
+
+# Omitting 'start' (starts from beginning)
+slice2 = text[:6] 
+print(slice2)  # Output: 'Python'
+
+# Omitting 'stop' (goes until the end)
+slice3 = text[7:]
+print(slice3)  # Output: 'Programming'
+
+
