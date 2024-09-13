c9e5194c0103caac715555f25f020a286570d5ed
MimFSD <mimislam878@gmail.com>
2024-09-13 21:23:43 +0600

    indexing

---
+++ Strings & Conditional Statements/indexing.py
@@ -0,0 +1,11 @@
+text = "Python"
+
+# Positive Indexing
+print(text[0])   # Output: 'P'
+print(text[1])   # Output: 'y'
+print(text[5])   # Output: 'n'
+
+# Negative Indexing
+print(text[-1])  # Output: 'n'
+print(text[-2])  # Output: 'o'
+print(text[-6])  # Output: 'P'
