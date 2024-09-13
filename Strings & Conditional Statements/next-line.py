48b5afb67d0cd743127e9432c857797dd21960a2
MimFSD <mimislam878@gmail.com>
2024-09-13 21:10:37 +0600

    new line

---
+++ Strings & Conditional Statements/next-line.py
@@ -0,0 +1,3 @@
+str4 = "Like every other programming language,\n Python also has some predefined conditional statements. A conditional statement as the name suggests itself, \n is used to handle conditions in your program. These statements guide the program while making \n decisions based on the conditions encountered by the program."
+
+print(str4)
---
+++ Strings & Conditional Statements/string.py
@@ -0,0 +1,5 @@
+str1= "The condition can be any expression that evaluates to a Boolean value (True or False). If the condition is True, the code block indented below the if statement will be executed. "
+
+str2 ='If the condition is False, the code block will be skipped.'
+
+str3 = """If the condition is True, the code block indented below the if statement will be executed """
