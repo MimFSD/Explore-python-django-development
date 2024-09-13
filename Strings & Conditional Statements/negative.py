a77594f21ad1a7fa76b1a56aee0ccbc90c66652e
MimFSD <mimislam878@gmail.com>
2024-09-13 21:27:14 +0600

    Negative Slicing

---
+++ Strings & Conditional Statements/negative.py
@@ -0,0 +1,12 @@
+# Slicing using negative indices
+slice4 = text[-11:]  # From index -11 to the end
+print(slice4)  # Output: 'Programming'
+
+slice5 = text[-18:-12]  # From index -18 to -13
+print(slice5)  # Output: 'Python'
+# Slicing using negative indices
+slice4 = text[-11:]  # From index -11 to the end
+print(slice4)  # Output: 'Programming'
+
+slice5 = text[-18:-12]  # From index -18 to -13
+print(slice5)  # Output: 'Python'
