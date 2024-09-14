8a25516628514dcb39fff452f549cd1a982fd705
MimFSD <mimislam878@gmail.com>
2024-09-14 22:10:24 +0600

    find & capatalize

--- Strings & Conditional Statements/String-Functions.py
+++ Strings & Conditional Statements/String-Functions.py
@@ -16,3 +16,9 @@ print(string.split(", "))  # Output: ['Hello', 'World!']
 
 words = ['Hello', 'World']
 print(" ".join(words))  # Output: "Hello World"
+
+string = "Hello, World!"
+print(string.find("World"))  # Output: 7
+
+string = "hello world"
+print(string.capitalize())  # Output: "Hello world"
