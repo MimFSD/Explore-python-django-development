be03616aee6f1e056cca0dd2dd0fee7bdc4ec85d
MimFSD <mimislam878@gmail.com>
2024-09-14 22:08:33 +0600

    Split & join

--- Strings & Conditional Statements/String-Functions.py
+++ Strings & Conditional Statements/String-Functions.py
@@ -8,3 +8,11 @@ print(len(string))  # Output: 13
 string = "Hello"
 print(string.upper())  # Output: "HELLO"
 print(string.lower())  # Output: "hello"
+
+
+string = "Hello, World!"
+print(string.split(", "))  # Output: ['Hello', 'World!']
+
+
+words = ['Hello', 'World']
+print(" ".join(words))  # Output: "Hello World"
